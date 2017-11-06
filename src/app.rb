59460541d1561690e1f476dac6d6f70e34dd8362
Marcos Alves <alves.henrique.marcos@gmail.com>
2017-11-05 22:32:29 -0300

    Adiciona rotas para cursos

--- src/app.rb
+++ src/app.rb
@@ -1,10 +1,32 @@
 require 'sinatra'
+require 'sinatra/contrib'
+require './controllers/curso'
 
 # set content type response application
 before do
   content_type 'application/json'
 end
 
+namespace '/api/v1' do
+  # routes cursos
+  curso_controller = CursoController.new
+  
+  get '/cursos' do
+    response = curso_controller.index
+    body response[:body]
+    status response[:status]
+  end
+  
+  get '/cursos/:id' do |id|
+    response = curso_controller.show(id)
+    body response[:body]
+    status response[:status]
+  end
+end
+
+# root
 get '/' do
-  { :API => "dados-sigaa-api", :version => '0.0.1' }.to_json
+  response =  { :status => 200, :body => { "API": "dados-sigaa-api", "version": "0.0.1" }.to_json }
+  body response[:body]
+  status response[:status]
 end
