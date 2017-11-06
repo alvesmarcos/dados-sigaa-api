37caca2eb0f2b346e8fcfd553908330e9d2aa9a5
Marcos Alves <alves.henrique.marcos@gmail.com>
2017-11-06 02:56:34 -0300

    Refatora model curso

---
+++ src/api/models/curso.rb
@@ -0,0 +1,12 @@
+class Curso
+  DB = ''
+  attr_accessor :id, :nome, :nivel, :centro, :modalidade
+
+  def initialize(id='', nome='', nivel='', centro='', modalidade='')
+    @id = id
+    @nome = nome
+    @nivel = nivel
+    @centro = centro
+    @modalidade = modalidade
+  end
+end
